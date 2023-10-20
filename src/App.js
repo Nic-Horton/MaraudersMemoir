92400284057e85a717f30b82be81d9ae40da3fdd
Nic-Horton <nic.horton00@gmail.com>
2023-10-19 20:04:02 -0500

    Added navbar with functionlaity to go to specific pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Characters from './pages/Characters';
 import Spells from './pages/Spells';
+import Navbar from './components/layout/Navbar';
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/characters" element={<Characters />}></Route>
