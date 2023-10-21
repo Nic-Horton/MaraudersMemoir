6e09898c989536c6d13938b1cf6e161dc9b65896
Nic-Horton <nic.horton00@gmail.com>
2023-10-21 13:31:53 -0500

    Took out api call temporarily

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,14 +1,5 @@
-import React, { useEffect } from 'react'
-
-
 function Characters() {
-
-  useEffect(()=>{
-    fetch('https://api.potterdb.com/v1/characters?filter[name_cont_any]=Harry')
-    .then((res)=> res.json())
-    .then((data)=>console.log(data))
-  },[])
-
+  
   return (
     <>
     <div>Characters</div>
