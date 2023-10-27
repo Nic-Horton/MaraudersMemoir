f8c95b580eb518be52b61edbb74dac6376581ba9
Nic-Horton <nic.horton00@gmail.com>
2023-10-26 22:09:25 -0500

    Added images to serve as borders for card lists

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,6 +1,6 @@
 import { getCharacters } from "../components/Data/fetch_characters";
 import CharacterList from "../components/ForPages/Characters/CharacterList";
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 
 //fetch call that will be passed to create the character list. Takes into account the page and searched name.
 const fetchCharacters = async ( { pageParam = 1, queryKey } ) => {
@@ -15,9 +15,33 @@ function Characters() {
   
   return (
     <>
+    <Paper
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          height: '100vh', 
+          width: '10%',
+          backgroundImage:"url(/images/prints1.PNG)",
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      />
     <Box width={'80%'} gap={2} sx={{mt:3,display:'flex',alignItems:'center',justifyContent:'stretch', flexDirection:'column'}}>
     <CharacterList  fetchCharacters={fetchCharacters}/>
     </Box>
+    <Paper
+        sx={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          height: '100vh', 
+          width: '10%',
+          backgroundImage:"url(/images/prints1.PNG)",
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      />
     </>
   )
 }
--- src/pages/Spells.jsx
+++ src/pages/Spells.jsx
@@ -1,6 +1,7 @@
 import { getSpells } from "../components/Data/fetch_spells";
 import SpellList from "../components/ForPages/Spells/SpellList";
-import { Box } from "@mui/material";
+import Box from "@mui/material/Box";
+import { Paper } from "@mui/material";
 
 //fetch call that will be passed to create the character list. Takes into account the page and searched name.
 const fetchSpells = async ( { pageParam = 1, queryKey } ) => {
@@ -15,9 +16,33 @@ const fetchSpells = async ( { pageParam = 1, queryKey } ) => {
 function Spells() {
   return (
     <>
+      <Paper
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          height: '100vh', 
+          width: '10%', 
+          backgroundImage:"url(/images/prints2.PNG)",
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      />
     <Box width={'80%'} gap={2} sx={{mt:3,display:'flex',alignItems:'center',justifyContent:'stretch', flexDirection:'column'}}>
     <SpellList  fetchSpells={fetchSpells}/>
     </Box>
+    <Paper
+        sx={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          height: '100vh', 
+          width: '10%',
+          backgroundImage:"url(/images/prints2.PNG)",
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      />
     </>
   )
 }
