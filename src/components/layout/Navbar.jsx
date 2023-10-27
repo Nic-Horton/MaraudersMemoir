db1799f0f8625fcc2d690d0a9d1220fc32a09224
Nic-Horton <nic.horton00@gmail.com>
2023-10-26 22:13:13 -0500

    Fixed punctuation error in Nav Drawer

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar(props) {
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
       <Typography variant="h6" sx={{ my: 2, fontFamily:'Lugrasimo'}}>
-      Marauder,s Memoir
+      Marauder's Memoir
       </Typography>
       <Divider />
       <List>
